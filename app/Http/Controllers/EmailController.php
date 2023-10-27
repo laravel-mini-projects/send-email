3fb388138d90bc97c58c5ec213dec3eeaef6219f
kamrunnaharruma <knahar12bd@gmail.com>
2023-10-27 11:26:26 +0600

    function to send email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendEmailJob;
 use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
     //
+
+    public function sendEmaill(){
+        SendEmailJob::dispatch('knahar12bd@gmail.com');
+
+        return "Mail has been sent.";
+    }
 }
