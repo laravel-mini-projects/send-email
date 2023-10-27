7473d698dd1fb1e8d1a3825067eb55a8b05bfcc1
kamrunnaharruma <knahar12bd@gmail.com>
2023-10-27 13:26:39 +0600

    route

--- app/Mail/SendEmail.php
+++ app/Mail/SendEmail.php
@@ -3,31 +3,38 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class SendEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $email;
+
     /**
      * Create a new message instance.
      *
+     * @param  string  $content
      * @return void
      */
-    public function __construct()
+    public function __construct($email)
     {
-        //
+        $this->email = $email;
     }
 
     /**
-     * Build the message.
+     * Build the email message.
      *
      * @return $this
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this
+            ->to('recipient@example.com') // Set the recipient's email address
+            ->subject('Subject of the Email') // Set the email subject
+            ->text('emails.send_email_plain_text'); // Set the plain text email view
     }
 }
+
